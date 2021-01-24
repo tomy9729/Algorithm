649ad178a2a33c0971452f8ea65700c81f78c45d
KimHyunsoo <tomy9729@naver.com>
2021-01-25 00:57:37 +0900

    Update 10. 재귀.py

--- BaekJoon/Step-by-step troubleshooting(단계별로 문제풀기)/10. 재귀.py
+++ BaekJoon/Step-by-step troubleshooting(단계별로 문제풀기)/10. 재귀.py
@@ -34,14 +34,14 @@ print(fibo(n))
 def star(n) :
   if n == 3 : 
     return [['*','*','*'],['*',' ','*'],['*','*','*']]
-  arr = [[0]*n for i in range(n)]
-  temp = star(n//3)
+  arr = [[0]*n for i in range(n)] #입력받은 크기만큼의 n*n 배열을 만든다.
+  temp = star(n//3) #시간초과를 회피하기 위함. 반복문 전에 재귀함수를 한 번만 수행하도록 한다.
   for i in range(n) : 
     for j in range(n) : 
-      if i >= n//3 and i < 2*n//3 and j >= n//3 and j < 2*n//3 : 
+      if i >= n//3 and i < 2*n//3 and j >= n//3 and j < 2*n//3 : #빈칸으로 두는 조건
         arr[i][j] = ' '
       else :
-        arr[i][j] = temp[i%(n//3)][j%(n//3)]
+        arr[i][j] = temp[i%(n//3)][j%(n//3)] #좌표에 해당하는 값만 가져온다.
   return arr
 
 n=int(input())
@@ -65,9 +65,9 @@ for i in range(n):
 n = int(input())
 c = 0
 
-def hanoi(n,s,d,v) : 
+def hanoi(n,s,d,v) : # n : 원판번호 s : 출발지 d : 도착지 v : 경유지
   if n == 1 : 
-    print("{} {}".format(s,d))
+    print("{} {}".format(s,d)) 
 
   else : 
     hanoi(n-1,s,v,d)
