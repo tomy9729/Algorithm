0b1fe2f9e9153c615f13ba5c4f1e48b0737f8b9e
KimHyunsoo <tomy9729@naver.com>
2021-02-02 02:32:32 +0900

    Update 14. 동적계획법1 (미완).py

--- BaekJoon/Step-by-step troubleshooting(단계별로 문제풀기)/14. 동적계획법1 (미완).py
+++ BaekJoon/Step-by-step troubleshooting(단계별로 문제풀기)/14. 동적계획법1 (미완).py
@@ -182,3 +182,60 @@ for i in range(1,n) :
     max_point[i][j] = triangle[i][j] + max(max_point[i-1][j-1],max_point[i-1][j])
 
 print(max(max_point[n-1]))
+
+#2579번 계단 오르기
+n = int(input()) 
+step = [] #계단
+for i in range(n) : 
+  step.append(int(input()))
+point = [False]*n
+if n == 1 : #계단이 하나
+   point[0] = step[0]
+elif n == 2 : #계단이 둘
+  point[1] = step[0] + step[1]
+else :  #계단이 셋 이상
+  point[0] = step[0]
+  point[1] = step[0] + step[1]
+  point[2] = max(step[1]+step[2],step[0]+step[2])
+  for i in range(3, n) : 
+    point[i] = step[i] + max(point[i-2], point[i-3] + step[i-1])
+
+print(point[n-1])
+
+#1463번 1로 만들기
+n = int(input())
+count = 0
+min_count = [0]*(n+1)
+def cal(n, count) : #재귀함수로 풀 경우 Recursion error 발생, 해답이 나오긴한다.
+  global min_count
+  if min_count[n] : 
+    return min_count[n]
+  if n == 1 : 
+    return count
+  if n % 6 == 0 : 
+    min_count[n] = min(cal(n//3,count+1),cal(n//2,count+1),cal(n-1,count+1))
+    return  min_count[n]
+  if n % 3 == 0 : 
+    min_count[n] = min(cal(n//3,count+1),cal(n-1,count+1))
+    return  min_count[n]
+  if n % 2 == 0 :
+    min_count[n] = min(cal(n//2, count+1),cal(n-1,count+1))
+    return  min_count[n]
+  min_count[n] = cal(n-1,count+1)
+  return min_count[n]
+
+min_count[1] = 0
+for i in range(2,n+1) :
+  if i % 6 == 0 : # 6의 배수일 때, 3으로 나눈 경우/2로 나눈 경우/1을 뺀 경우 중 최솟값
+    min_count[i] = 1 + min(min_count[i//3],min_count[i//2],min_count[i-1]) 
+    continue
+  if i % 3 == 0 : # 3의 배수일 때, 3으로 나눈 경우/1을 뺀 경우 중 최솟값
+    min_count[i] = 1 + min(min_count[i//3],min_count[i-1])
+    continue
+  if i % 2 == 0 : # 2의 배수 일 때, 2로 나눈 경우, 1을 뺀 경우 중 최솟값
+    min_count[i] = 1 + min(min_count[i//2],min_count[i-1])
+    continue
+  min_count[i] = min_count[i-1] +1 # 2,3의 배수가 아닌경우 그냥 1을 뺀다
+
+#print(cal(n,0))  # 재귀함수로 풀 경우 답 출력
+print(min_count[n])
