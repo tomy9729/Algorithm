91bbe59d74b84268a71a09337a4067a98d274ec9
KimHyunsoo <tomy9729@naver.com>
2021-02-04 02:57:11 +0900

    Update 14. 동적계획법1 (미완).py

--- BaekJoon/Step-by-step troubleshooting(단계별로 문제풀기)/14. 동적계획법1 (미완).py
+++ BaekJoon/Step-by-step troubleshooting(단계별로 문제풀기)/14. 동적계획법1 (미완).py
@@ -239,3 +239,19 @@ for i in range(2,n+1) :
 
 #print(cal(n,0))  # 재귀함수로 풀 경우 답 출력
 print(min_count[n])
+
+#10844번 쉬운 계단 수
+n = int(input())
+
+step_num = [[0]*(10) for i in range(n+1)]
+for i in range(1,10) : 
+  step_num[1][i] = 1
+
+for i in range(2,n+1) : 
+  step_num[i][0] = step_num[i-1][1] # 1의 자리가 0인 수를 만드는 것은 1의 자리가 1인 수
+  step_num[i][9] = step_num[i-1][8] # 1의 자리가 9인 수를 만드는 것은 1의 자리가 8인 수
+  for j in range(1,9) : 
+    step_num[i][j] = (step_num[i-1][j-1] + step_num[i-1][j+1])%1000000000 # 1의 자리가 j인 수를 만드는 것은 1의 자리가 j-1, j+1인 수
+
+print(sum(step_num[n])%1000000000)
+
