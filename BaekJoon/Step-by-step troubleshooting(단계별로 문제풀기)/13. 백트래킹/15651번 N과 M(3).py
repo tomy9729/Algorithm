5c0bef456390023d082c809ee8edd151a9f4e0ea
KimHyunsoo <tomy9729@naver.com>
2021-02-05 05:15:37 +0900

    Create 15651번 N과 M(3).py

---
+++ BaekJoon/Step-by-step troubleshooting(단계별로 문제풀기)/13. 백트래킹/15651번 N과 M(3).py
@@ -0,0 +1,19 @@
+#15651번 N과 M(3)
+n,m = input().split()
+n = int(n)
+m = int(m)
+
+arr = [0]*(m+1)
+
+def check(x) :
+  if x == m+1 : 
+    for i in range(1,m+1) : 
+      print(arr[i], end=' ')
+    print()
+  else : 
+    for i in range(1,n+1) : 
+      arr[x] = i
+      check(x+1)
+      arr[x] = 0
+
+check(1)
