a0b51b9d5ff805331866bdcf26b676f1ebeb5a76
KimHyunsoo <tomy9729@naver.com>
2021-02-05 05:15:24 +0900

    Create 15650번 N과 M(2).py

---
+++ BaekJoon/Step-by-step troubleshooting(단계별로 문제풀기)/13. 백트래킹/15650번 N과 M(2).py
@@ -0,0 +1,20 @@
+#15650번 N과 M(2)
+n,m = input().split()
+n = int(n)
+m = int(m)
+
+arr = [0]*(m+1)
+
+def check(x) :
+  if x == m+1 : 
+    for i in range(1,m+1) : 
+      print(arr[i], end=' ')
+    print()
+  else : 
+    for i in range(1,n+1) : 
+      if max(arr) < i : 
+        arr[x] = i
+        check(x+1)
+        arr[x] = 0
+
+check(1)
