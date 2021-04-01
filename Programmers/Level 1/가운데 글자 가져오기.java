c014d4aa9d921c85ea1be0f36e4671ca062b5530
KimHyunsoo <tomy9729@naver.com>
2021-04-01 16:47:59 +0900

    Create 가운데 글자 가져오기.java

---
+++ Programmers/Level 1/가운데 글자 가져오기.java
@@ -0,0 +1,13 @@
+//Level 1 가운데 글자 가져오기.java
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        if(s.length()%2 == 1){
+            answer = s.substring(s.length()/2,s.length()/2+1); //문자열을 슬라이싱
+        }
+        else{
+            answer = s.substring(s.length()/2-1,s.length()/2+1);
+        }
+        return answer;
+    }
+}
