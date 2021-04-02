3af3c1b1f585bb0d83f91ce53dd2ae9d816af94b
KimHyunsoo <tomy9729@naver.com>
2021-04-02 17:39:44 +0900

    Create 자릿수 더하기.java

---
+++ Programmers/Level 1/자릿수 더하기.java
@@ -0,0 +1,13 @@
+//Level 1 자릿수 더하기.java
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(n != 0){
+            answer+=n%10;
+            n = n/10;
+        }
+        return answer;
+    }
+}
