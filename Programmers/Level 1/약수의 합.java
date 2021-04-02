df719b38b593777a941a0772a1ae7700aa93cfb1
KimHyunsoo <tomy9729@naver.com>
2021-04-02 14:10:53 +0900

    Update 약수의 합.java

--- Programmers/Level 1/약수의 합.java
+++ Programmers/Level 1/약수의 합.java
@@ -2,11 +2,9 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for(int i=1;i<n+1;i++){//1부터 n까지 모둔 수에 대하여
-            if(n%i==0){//나눠지면
-                answer+=i;//약수
-            }
-        }
-        return answer;
+        for(int i=1;i<=n/2;i++){
+            if(n%i==0)answer+=i;
+            
+        return answer+n;
     }
 }
