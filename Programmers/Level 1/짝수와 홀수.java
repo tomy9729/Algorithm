246296165bfeb73a112b8f01833dd353fc0d7b00
KimHyunsoo <tomy9729@naver.com>
2021-04-03 12:04:01 +0900

    Create 짝수와 홀수.java

---
+++ Programmers/Level 1/짝수와 홀수.java
@@ -0,0 +1,10 @@
+//Level 1 짝수와 홀수.java
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+        if(num%2==0) answer = "Even";
+        else answer = "Odd";
+        
+        return answer;
+    }
+}
