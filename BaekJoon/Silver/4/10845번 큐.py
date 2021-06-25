ad283ff8bae4b7906ae1d442348a50b8045f0ea4
KimHyunsoo <tomy9729@naver.com>
2021-06-26 08:00:00 +0900

    Create 10845번 큐.py

---
+++ BaekJoon/Silver/4/10845번 큐.py
@@ -0,0 +1,31 @@
+#10845번 큐.py
+from collections import deque
+if __name__ == "__main__":
+  q = deque()
+  n = int(input())
+  for i in range(n) : 
+    instruntion = input()[:-1]
+    if instruntion[:4] == "push" : 
+      q.append(int(instruntion[5:]))
+    elif instruntion == "pop" : 
+      if len(q) == 0 : 
+        print(-1)
+      else :
+        print(q.popleft())
+    elif instruntion == "size" : 
+      print(len(q))
+    elif instruntion == "empty" : 
+      if len(q) == 0 :
+        print(1)
+      else : 
+        print(0)
+    elif instruntion == "front" : 
+      if len(q) == 0 :
+        print(-1)
+      else :
+        print(q[0])
+    elif instruntion == "back" : 
+      if len(q)==0:
+        print(-1)
+      else :
+        print(q[-1])
