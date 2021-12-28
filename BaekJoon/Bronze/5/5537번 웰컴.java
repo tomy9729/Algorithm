6882d177366f4eb7d216ad68adeab3356de936cf
KimHyunsoo <tomy9729@naver.com>
2021-12-28 22:05:04 +0900

    Create 5537번 웰컴.java

---
+++ BaekJoon/Bronze/5/5537번 웰컴.java
@@ -0,0 +1,11 @@
+//5537번 웰컴.java
+public class 웰컴 {
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) {
+		sb.append(".  .   .\n");
+		sb.append("|  | _ | _. _ ._ _  _\n");
+		sb.append("|/\\|(/.|(_.(_)[ | )(/.\n");
+		
+		System.out.println(sb.toString());
+	}
+}
